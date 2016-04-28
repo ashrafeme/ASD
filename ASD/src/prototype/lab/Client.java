57ddb9238b5f81983e5215077b3d1f7f3ef038c3
ashrafeme <ashraf_eme@hotmail.com>
2016-04-28 01:27:23 -0500

    prototype lab

--- ASD/src/prototype/lab/Client.java
+++ ASD/src/prototype/lab/Client.java
@@ -1,7 +1,5 @@
 package prototype.lab;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class Client {
@@ -9,34 +7,40 @@ public class Client {
 	public static void main(String[] args) {
 
 		Employee emp = createRandomEmployee();
-
+		emp.setStaff(createRandomStaff(emp));
+		emp.setSupervisor(createRandomEmployee());
 		try {
 			Employee emp1 = (Employee) emp.clone();
 			System.out.println(emp);
-			emp1.setId(50);
-			System.out.println(emp);
+			//emp1.setId(50);
+			System.out.println(emp1);
+			for (Employee staffEmp : emp1.getStaff()) {
+				System.out.println(staffEmp);
+			}
 		} catch (CloneNotSupportedException e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	public static Employee createRandomEmployee() {
-
+		Employee emp = new Employee();
 		Random rand = new Random();
-		int id = rand.nextInt(500000);
-
-		return new Employee(id);
+		emp.setId(rand.nextInt(500000));
+		emp.setStreetAddress("1000 N 4th");
+		emp.setFirstname("Ashraf");
+		emp.setLastname("Ezzat");
+		emp.setCity("Fairfield");
+		emp.setState("IA");
+		emp.setZipcode("52557");
+		return emp;
 	}
 
-	public static Employee[] createRandomStaff() {
-
-		List<Employee> olist = new ArrayList<>();
-		for (int i = 0; i < 10; i++) {
-			olist.add(createRandomEmployee());
+	public static Employee[] createRandomStaff(Employee supervisor) {
+		Employee[] olist = new Employee[10];
+		for (int i = 0; i < olist.length; i++) {
+			olist[i] = createRandomEmployee();
+			olist[i].setSupervisor(supervisor);
 		}
-
-		return (Employee[]) olist.toArray();
-
+		return olist;
 	}
 }
--- ASD/src/prototype/lab/Employee.java
+++ ASD/src/prototype/lab/Employee.java
@@ -5,6 +5,13 @@ import java.io.Serializable;
 public class Employee implements Cloneable, Serializable {
 
 	private int id;
+	private String Lastname;
+	private String Firstname;
+	private String streetAddress;
+	private String city;
+	private String state;
+	private String zipcode;
+
 	private Employee supervisor;
 	private Employee staff[];
 
@@ -38,10 +45,10 @@ public class Employee implements Cloneable, Serializable {
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
 		Employee newEmployee = new Employee();
-		newEmployee.id = id;
+		copyEmployeeData(newEmployee, this);
 
 		if (supervisor != null)
-			newEmployee.supervisor = (Employee) supervisor.clone();
+			newEmployee.supervisor = cloneSupervisor(supervisor);
 		if (staff != null && staff.length > 0) {
 			newEmployee.staff = new Employee[staff.length];
 			for (int i = 0; i < staff.length; i++) {
@@ -49,6 +56,22 @@ public class Employee implements Cloneable, Serializable {
 			}
 		}
 		return newEmployee;
+	}
+
+	private Employee cloneSupervisor(Employee supervisor) {
+		Employee newSuper = new Employee();
+		copyEmployeeData(newSuper, supervisor);
+		return newSuper;
+	}
+
+	private void copyEmployeeData(Employee newEmployee, Employee employee) {
+		newEmployee.id = employee.getId();
+		newEmployee.Lastname = employee.getLastname();
+		newEmployee.Firstname = employee.getFirstname();
+		newEmployee.streetAddress = employee.getStreetAddress();
+		newEmployee.city = employee.getCity();
+		newEmployee.state = employee.getState();
+		newEmployee.zipcode = employee.getZipcode();
 	}
 
 	public int getId() {
@@ -59,10 +82,81 @@ public class Employee implements Cloneable, Serializable {
 		this.id = id;
 	}
 
+	public String getLastname() {
+		return Lastname;
+	}
+
+	public void setLastname(String lastname) {
+		Lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return Firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		Firstname = firstname;
+	}
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public Employee getSupervisor() {
+		return supervisor;
+	}
+
+	public void setSupervisor(Employee supervisor) {
+		this.supervisor = supervisor;
+	}
+
+	public Employee[] getStaff() {
+		return staff;
+	}
+
+	public void setStaff(Employee[] staff) {
+		this.staff = staff;
+	}
+
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return "Id:" + id + " - SuperVisor:" + supervisor;
+		StringBuilder osb = new StringBuilder();
+
+		osb.append("RefrenceId:" + System.identityHashCode(this) + "\n");
+		osb.append("Id:" + id + "\n");
+		osb.append("FullName:" + Firstname + " " + Lastname + "\n");
+		osb.append("Address:" + streetAddress + "\n");
+		osb.append(city + "," + state + " " + zipcode + "\n");
+		osb.append("SuperVisor:" + supervisor + "\n");
+		return osb.toString();
 	}
 
 	/**
