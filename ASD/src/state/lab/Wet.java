371b8fa86be4b6b740f350ef40e5a78c2448da87
ashrafeme <ashraf_eme@hotmail.com>
2016-05-11 01:38:09 -0500

    state lab

---
+++ ASD/src/state/lab/Wet.java
@@ -0,0 +1,29 @@
+package state.lab;
+
+public class Wet implements RoadState {
+
+	@Override
+	public void left() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void accel() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void right() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void brake() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
